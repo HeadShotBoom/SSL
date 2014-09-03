5906bfbe43b54e6e4f58129c0a2b43d338c78e70
Daniel Carroll <DCarroll@fullsail.edu>
2014-09-02 21:42:25 -0500

    Array video 1 done

--- Week 1/sandbox/arrays.php
+++ Week 1/sandbox/arrays.php
@@ -3,6 +3,38 @@
     <title>Arrays</title>
 </head>
 <body>
+<?php
+
+$numbers = array(4,8,15,16,23,42);
+echo $numbers[1]
+
+?>
+
+<?php $mixed = array(6, "fox", "dog", array("x", "y", "z")); ?><br />
+<?php echo $mixed[2]; ?><br />
+
+
+<!--THIS DOSENT WORK!!!-->
+<?php // echo $mixed[3]; ?><br />
+<?php // echo $mixed; ?><br />
+
+
+<!--This does-->
+<?php echo $mixed[3][1]; ?><br />
+
+<?php $mixed[2] = "cat"; ?>
+<?php $mixed[4] = "mouse"; ?>
+<?php $mixed[] = "horse"; ?>
+
+
+<pre>
+<?php echo print_r($mixed); ?><br />
+</pre>
+
+<?php
+//5.4 added the short syntax
+$array = [1,2,3];
+?>
 
 </body>
 </html>
