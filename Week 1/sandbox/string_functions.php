9ac56da2fa6b0266810e888557478f3d2ba129ee
Daniel Carroll <DCarroll@fullsail.edu>
2014-09-02 20:47:34 -0500

    String Functions

--- Week 1/sandbox/string_functions.php
+++ Week 1/sandbox/string_functions.php
@@ -20,6 +20,17 @@ Lowercase: <?php echo strtolower($third); ?><br />
 Uppercase: <?php echo strtoupper($third); ?><br />
 Uppercase First: <?php echo ucfirst($third); ?><br />
 Uppercase Words: <?php echo ucwords($third); ?><br />
+<br />
+Length: <?php echo strlen($third); ?><br />
+Trim: <?php echo "A" . trim(" B C D "). "E"; ?><br />
+Find: <?php echo strstr($third, "brown"); ?><br />
+Replace by string: <?php echo str_replace("quick", "super-fast", $third); ?><br />
+<br />
+Repeat: <?php echo str_repeat($third, 2); ?><br />
+Make Substring: <?php echo substr($third, 5 ,10); ?><br />
+Find Position: <?php echo strpos($third, "brown"); ?><br />
+Find Character: <?php echo strchr($third, "y"); ?><br />
+
 
 
 </body>
