8df19d840533264837ba83ec1ac14cbfa9d99198
Clayton Allen <Clayton@Claytons-MacBook-Pro.local>
2014-09-07 14:57:51 -0500

    Activity PHP Sessions Initial Commit

---
+++ Week 2/Act-PHP-Sessions/getsessions.php
@@ -0,0 +1,3 @@
+<?php
+// Retrieve an existing session and echo out the session variables you set in setsession.
+?>
---
+++ Week 2/Act-PHP-Sessions/setsessions.php
@@ -0,0 +1,3 @@
+<?php
+// Start a session and set session variables based on GET parameters you pass through the querystring. Set at least 3 variables.
+?>
