5257bf99b8ce6a477d6ec4fee1f121cce4c10141
Clayton Allen <Clayton@Claytons-MacBook-Pro.local>
2014-09-07 21:29:37 -0500

    Sessions
    Need more work I think. The Variables do need persist.

--- Week 2/Act-PHP-Sessions/getsessions.php
+++ Week 2/Act-PHP-Sessions/getsessions.php
@@ -1,3 +1,7 @@
 <?php
 // Retrieve an existing session and echo out the session variables you set in setsession.
+
+echo $_SESSION["name"];
+echo $_SESSION["state"];
+echo $_SESSION["city"];
 ?>
--- Week 2/Act-PHP-Sessions/setsessions.php
+++ Week 2/Act-PHP-Sessions/setsessions.php
@@ -1,3 +1,13 @@
 <?php
 // Start a session and set session variables based on GET parameters you pass through the querystring. Set at least 3 variables.
+
+session_start();
+
+$_SESSION["name"]  = $_GET["name"];
+$_SESSION["state"] = $_GET["state"];
+$_SESSION["city"]  = $_GET["city"];
+echo $_SESSION["name"];
+echo $_SESSION["state"];
+echo $_SESSION["city"];
+
 ?>
