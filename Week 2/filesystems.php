8068258831e8b3f3fa2fc349d9361b68e602b1d8
Clayton Allen <Clayton@Claytons-MacBook-Pro.local>
2014-09-08 20:41:15 -0500

    PDO Pre Build pt.1
    The Last ACT for this week so far

---
+++ Week 2/filesystems.php
@@ -0,0 +1,15 @@
+<?php
+
+echo __FILE__ . "<br>";
+
+echo __LINE__ . "<br>";
+
+echo dirname(__FILE__);
+
+echo __DIR__ . "<br>";
+
+echo file_exists(__FILE__) ? 'yes' : 'no';
+
+echo "string";
+
+?>
